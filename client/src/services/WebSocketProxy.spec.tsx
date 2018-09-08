c5a7c732cae43a170f85bc0b952bef9ec583231f
Quirino Lo Russo <quirimmo@gmail.com>
2018-09-08 12:11:09 +0200

    added all unit tests

--- client/src/services/WebSocketProxy.spec.tsx
+++ client/src/services/WebSocketProxy.spec.tsx
@@ -1,8 +1,34 @@
-import WebSocketProxy from "./WebSocketProxy";
+const mockSocketIO = jest.fn();
+jest.mock('socket.io-client', () => mockSocketIO);
+
+import WebSocketProxy from './WebSocketProxy';
+import * as rxjs from 'rxjs';
+import DailySerie from './../models/DailySerie';
+import { SERVER_URL } from './../constants/constants';
 
 const instance: WebSocketProxy = new WebSocketProxy();
 
 describe('WebSocketProxy', () => {
+	const luffyMessage = {
+		finished: false,
+		data: {
+			symbol: 'Symbol',
+			lastMovement: 0,
+			priceChange: [1, 2],
+			trend: 3
+		}
+	};
+	const mockEmit = jest.fn();
+	const mockDisconnect = jest.fn();
+	beforeEach(() => {
+		spyOn(WebSocketProxy, 'getSocket').and.returnValue({ emit: mockEmit, id: 'socket', disconnect: mockDisconnect });
+		spyOn(WebSocketProxy, 'getStreamObservable').and.returnValue(new rxjs.Subject<any>());
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
 	it('should be defined', () => {
 		expect(WebSocketProxy).toBeDefined();
 	});
@@ -22,5 +48,116 @@ describe('WebSocketProxy', () => {
 		expect(typeof WebSocketProxy.onLuffyMessageSubscribe).toEqual('function');
 		expect(typeof WebSocketProxy.requestAllData).toEqual('function');
 		expect(typeof WebSocketProxy.sendMessage).toEqual('function');
+		expect(typeof WebSocketProxy.subscribeToSocketEvents).toEqual('function');
+	});
+
+	describe('requestAllData', () => {
+		it('should emit the message with the right parameters', () => {
+			WebSocketProxy.requestAllData(5);
+			expect(mockEmit).toHaveBeenCalledWith('luffy-message', { action: 'getAllData', size: 5 });
+		});
+	});
+
+	describe('sendMessage', () => {
+		it('should emit the message with the right parameters', () => {
+			WebSocketProxy.sendMessage('keyword', 'data');
+			expect(mockEmit).toHaveBeenCalledWith('keyword', 'data');
+		});
+	});
+
+	describe('disconnect', () => {
+		it('should call the disconnect method of the socket', () => {
+			WebSocketProxy.disconnect();
+			expect(mockDisconnect).toHaveBeenCalled();
+		});
+	});
+
+	describe('onLuffyMessageSubscribe', () => {
+		it('should log the received message', () => {
+			spyOn(console, 'log');
+			WebSocketProxy.onLuffyMessageSubscribe(luffyMessage);
+			expect(console.log).toHaveBeenCalledWith('Client: luffy message received', luffyMessage);
+		});
+
+		it('should emit a message of the streamObservable', () => {
+			const spy = spyOn(WebSocketProxy.getStreamObservable(), 'next');
+			WebSocketProxy.onLuffyMessageSubscribe(luffyMessage);
+			const dailySerie: DailySerie = new DailySerie('Symbol', 0, [1, 2], 3);
+			expect(spy).toHaveBeenCalledWith({ dailySerie, finished: false });
+		});
+	});
+
+	describe('onGeneralMessageSubscribe', () => {
+		it('should log the received message', () => {
+			spyOn(console, 'log');
+			WebSocketProxy.onGeneralMessageSubscribe('message');
+			expect(console.log).toHaveBeenCalledWith('Client: message received', 'message');
+		});
+	});
+
+	describe('onDisconnectSubscribe', () => {
+		it('should log the disconnected socket', () => {
+			spyOn(console, 'log');
+			WebSocketProxy.onDisconnectSubscribe();
+			expect(console.log).toHaveBeenCalledWith('Socket client disconnected with id: ', 'socket');
+		});
+	});
+
+	describe('onConnectSubscribe', () => {
+		it('should log the connected socket', () => {
+			spyOn(console, 'log');
+			WebSocketProxy.onConnectSubscribe();
+			expect(console.log).toHaveBeenCalledWith('Socket client connected with id: ', 'socket');
+		});
+	});
+
+	describe('connect', () => {
+		it('should instantiate the client socket', () => {
+			WebSocketProxy.connect();
+			expect(mockSocketIO).toHaveBeenCalledWith(SERVER_URL);
+		});
+
+		it('should call the subscribeToSocketEvents method', () => {
+			const spy = spyOn(WebSocketProxy, 'subscribeToSocketEvents');
+			WebSocketProxy.connect();
+			expect(spy).toHaveBeenCalled();
+		});
+
+		it('should return an observable of the connection event', () => {
+			expect(WebSocketProxy.connect() instanceof rxjs.Observable).toBeTruthy();
+		});
+	});
+
+	describe('subscribeToSocketEvents', () => {
+		let mockFromEvent: any;
+		let mockFromEventSubscription: any;
+		beforeEach(() => {
+			mockFromEvent = spyOn(rxjs, 'fromEvent').and.returnValue(new rxjs.Observable());
+			mockFromEventSubscription = spyOn(mockFromEvent(), 'subscribe');
+		});
+
+		it('should subscribe to the socket connect event', () => {
+			WebSocketProxy.subscribeToSocketEvents();
+			expect(mockFromEvent).toHaveBeenCalledWith(WebSocketProxy.getSocket(), 'connect');
+			expect(mockFromEventSubscription).toHaveBeenCalledWith(WebSocketProxy.onConnectSubscribe);
+		});
+
+		it('should subscribe to the socket disconnect event', () => {
+			WebSocketProxy.subscribeToSocketEvents();
+			expect(mockFromEvent).toHaveBeenCalledWith(WebSocketProxy.getSocket(), 'disconnect');
+			expect(mockFromEventSubscription).toHaveBeenCalledWith(WebSocketProxy.onDisconnectSubscribe);
+		});
+
+		it('should subscribe to the socket message event', () => {
+			WebSocketProxy.subscribeToSocketEvents();
+			expect(mockFromEvent).toHaveBeenCalledWith(WebSocketProxy.getSocket(), 'message');
+			expect(mockFromEventSubscription).toHaveBeenCalledWith(WebSocketProxy.onGeneralMessageSubscribe);
+		});
+
+		it('should subscribe to the socket luffy message event', () => {
+			WebSocketProxy.subscribeToSocketEvents();
+			expect(mockFromEvent).toHaveBeenCalledWith(WebSocketProxy.getSocket(), 'luffy-message');
+			expect(mockFromEventSubscription).toHaveBeenCalledWith(WebSocketProxy.onLuffyMessageSubscribe);
+		});
 	});
 });
--- client/src/services/WebSocketProxy.tsx
+++ client/src/services/WebSocketProxy.tsx
@@ -1,6 +1,6 @@
 import * as io from 'socket.io-client';
 import { SERVER_URL } from './../constants/constants';
-import { fromEvent, Observable, Observer, Subject, of } from 'rxjs';
+import { fromEvent, Observable, Subject, of } from 'rxjs';
 import DailySerie from './../models/DailySerie';
 
 class WebSocketProxy {
@@ -11,27 +11,30 @@ class WebSocketProxy {
 		return WebSocketProxy.socket;
 	}
 
-	public static getStreamObservable(): Observable<any> {
+	public static getStreamObservable(): Subject<any> {
 		return WebSocketProxy.streamObservable;
 	}
 
 	public static connect(): Observable<any> {
 		WebSocketProxy.streamObservable = new Subject<any>();
 		WebSocketProxy.socket = io(SERVER_URL);
-		fromEvent(WebSocketProxy.socket, 'connect').subscribe(WebSocketProxy.onConnectSubscribe);
-		fromEvent(WebSocketProxy.socket, 'disconnect').subscribe(WebSocketProxy.onDisconnectSubscribe);
-		fromEvent(WebSocketProxy.socket, 'message').subscribe(WebSocketProxy.onGeneralMessageSubscribe);
-		fromEvent(WebSocketProxy.socket, 'luffy-message').subscribe(WebSocketProxy.onLuffyMessageSubscribe);
+		WebSocketProxy.subscribeToSocketEvents();
+		return fromEvent(WebSocketProxy.getSocket(), 'connect');
+	}
 
-		return fromEvent(WebSocketProxy.socket, 'connect');
+	public static subscribeToSocketEvents() {
+		fromEvent(WebSocketProxy.getSocket(), 'connect').subscribe(WebSocketProxy.onConnectSubscribe);
+		fromEvent(WebSocketProxy.getSocket(), 'disconnect').subscribe(WebSocketProxy.onDisconnectSubscribe);
+		fromEvent(WebSocketProxy.getSocket(), 'message').subscribe(WebSocketProxy.onGeneralMessageSubscribe);
+		fromEvent(WebSocketProxy.getSocket(), 'luffy-message').subscribe(WebSocketProxy.onLuffyMessageSubscribe);
 	}
 
 	public static onConnectSubscribe(): void {
-		console.log('Socket client connected with id: ', WebSocketProxy.socket.id);
+		console.log('Socket client connected with id: ', WebSocketProxy.getSocket().id);
 	}
 
 	public static onDisconnectSubscribe(): void {
-		console.log('Socket client disconnected with id: ', WebSocketProxy.socket.id);
+		console.log('Socket client disconnected with id: ', WebSocketProxy.getSocket().id);
 	}
 
 	public static onGeneralMessageSubscribe(message: any): void {
@@ -40,25 +43,29 @@ class WebSocketProxy {
 
 	public static onLuffyMessageSubscribe(message: any): void {
 		console.log('Client: luffy message received', message);
-		if (message.finished === true) {
-			WebSocketProxy.streamObservable.next({ finished: true });
-		} else {
-			const rawData: any = message.data;
-			const dailySerie: DailySerie = new DailySerie(rawData.symbol, rawData.lastMovement, rawData.priceChange, rawData.trend);
-			WebSocketProxy.streamObservable.next(dailySerie);
-		}
+		const rawData: any = message.data;
+		const dailySerie: DailySerie = new DailySerie(rawData.symbol, rawData.lastMovement, rawData.priceChange, rawData.trend);
+		WebSocketProxy.getStreamObservable().next({ dailySerie, finished: message.finished });
+
+		// if (message.finished === true) {
+		// 	WebSocketProxy.getStreamObservable().next({ finished: true });
+		// } else {
+		// 	const rawData: any = message.data;
+		// 	const dailySerie: DailySerie = new DailySerie(rawData.symbol, rawData.lastMovement, rawData.priceChange, rawData.trend);
+		// 	WebSocketProxy.getStreamObservable().next(dailySerie);
+		// }
 	}
 
 	public static disconnect(): Observable<any> {
-		return of(WebSocketProxy.socket.disconnect());
+		return of(WebSocketProxy.getSocket().disconnect());
 	}
 
 	public static sendMessage(keyword: string, data: any) {
-		WebSocketProxy.socket.emit(keyword, data);
+		WebSocketProxy.getSocket().emit(keyword, data);
 	}
 
 	public static requestAllData(size?: number) {
-		WebSocketProxy.socket.emit('luffy-message', { action: 'getAllData', size });
+		WebSocketProxy.getSocket().emit('luffy-message', { action: 'getAllData', size });
 	}
 }
 
