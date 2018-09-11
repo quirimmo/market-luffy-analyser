a3b3c5d8b3ab6e29a94f326a420cb51f57c5d2b1
Quirino Lo Russo <quirimmo@gmail.com>
2018-09-12 01:14:12 +0200

    going on with unit tests

--- client/src/components/companies/Companies.container.spec.tsx
+++ client/src/components/companies/Companies.container.spec.tsx
@@ -4,13 +4,16 @@ import configureMockStore from 'redux-mock-store';
 
 import CompaniesPage from './Companies.container';
 import Company from './../../models/Company';
+import thunk from 'redux-thunk';
 
-const mockStore = configureMockStore();
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 const company: Company = new Company('Symbol 1', 'Company 1', 1, 2, 'Sector 1', 'Industry 1');
 const companies: Company[] = [company];
 const store = mockStore({
 	companies
 });
+
 let component: ShallowWrapper<any, any>;
 
 describe('Companies Container Component', () => {
@@ -22,19 +25,23 @@ describe('Companies Container Component', () => {
 		expect(component).toBeDefined();
 	});
 
-	// it('should init the companies prop of the store', () => {
-	// 	expect(component.props().companies).toEqual(companies);
-	// });
+	it('should init the companies prop of the store', () => {
+		expect(component.props().companies).toEqual(companies);
+	});
 
-	// it('should define the fetchCompanies prop', () => {
-	// 	expect(typeof component.props().fetchCompanies).toEqual('function');
-	// });
+	it('should define the fetchCompanies prop', () => {
+		expect(typeof component.props().fetchCompanies).toEqual('function');
+	});
 
-	// describe('fetchCompanies', () => {
-	// 	it('should dispatch the FETCH_COMPANIES action', () => {
-	// 		const expectedAction = { type: 'FETCH_COMPANIES' };
-	// 		component.props().fetchCompanies();
-	// 		expect(store.getActions()).toContainEqual(expectedAction);
-	// 	});
-	// });
+	describe('fetchCompanies', () => {
+		it('should dispatch the FETCH_COMPANIES action', () => {
+			const expectedAction = { type: 'FETCH_COMPANIES' };
+			component
+				.props()
+				.fetchCompanies()
+				.subscribe(() => {
+					expect(store.getActions()).toContainEqual(expectedAction);
+				});
+		});
+	});
 });
