b5d568fe3f12b50cf4c4167c438bbb21fd7c4434
Quirino Lo Russo <quirimmo@gmail.com>
2018-09-12 17:14:58 +0200

    added unit tests to Home presentational component

--- client/src/components/home/Home.component.spec.tsx
+++ client/src/components/home/Home.component.spec.tsx
@@ -2,17 +2,22 @@ import * as React from 'react';
 import { shallow, mount } from 'enzyme';
 import Home from './Home.component';
 import DailySerie from './../../models/DailySerie';
+import { Button } from 'reactstrap';
+import DailySeries from '../daily-series/DailySeries.component';
 
 let component: any;
-const mockFetchDailySeries = jest.fn(() => {});
-const mockResetDailySeries = jest.fn(() => {});
-const inputDailySeries: DailySerie[] = [];
+const mockFetchDailySeries = jest.fn((num: number, callback: () => {}) => {
+	callback();
+});
+const mockResetDailySeries = jest.fn();
+const dailySerie1 = new DailySerie('SYMBOL1', 1, [2, 3], 4);
+const dailySerie2 = new DailySerie('SYMBOL2', 5, [6, 7], 8);
+const inputDailySeries: DailySerie[] = [dailySerie1, dailySerie2];
 
 describe('Home Presentational Component', () => {
 	beforeEach(() => {
 		component = shallow(<Home dailySeries={inputDailySeries} fetchDailySeries={mockFetchDailySeries} resetDailySeries={mockResetDailySeries} />);
 	});
-
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
@@ -25,47 +30,43 @@ describe('Home Presentational Component', () => {
 		expect(typeof component.instance().getAllData).toEqual('function');
 	});
 
-	describe('empty daily series', () => {
-		it('should define the loading daily series message', () => {
-			const dailySeriesMessage = component.find('.daily-series-message');
-			expect(dailySeriesMessage).toHaveLength(1);
-			expect(dailySeriesMessage.text()).toEqual('Loading daily series...');
-		});
+	it('should init the state', () => {
+		expect(component.state()).toEqual({ isLoadingData: false });
 	});
 
-	describe('button getAllData', () => {
-		let buttonGetAllData: any;
-
-		beforeEach(() => {
-			buttonGetAllData = component.find('button');
+	describe('render', () => {
+		it('should display the Button component for getting all data', () => {
+			const button = component.find(Button);
+			expect(button).toHaveLength(1);
+			expect(button.props().disabled).toBeFalsy();
+			expect(button.props().onClick).toEqual(component.instance().getAllData);
+			expect(button.props().children).toEqual('GET ALL DATA');
 		});
 
-		it('should be defined', () => {
-			expect(buttonGetAllData).toHaveLength(1);
-		});
-
-		it('should have the proper text', () => {
-			expect(buttonGetAllData.text()).toEqual('GET ALL DATA');
-		});
-
-		it('should call the getAllData method on click', () => {
-			const spy = spyOn(Home.prototype, 'getAllData');
-			const wrapper = mount(<Home dailySeries={inputDailySeries} fetchDailySeries={mockFetchDailySeries} resetDailySeries={mockResetDailySeries} />);
-			expect(spy).not.toHaveBeenCalled();
-			wrapper.find('button').simulate('click');
-			expect(spy).toHaveBeenCalled();
+		it('should display the DailySeries component', () => {
+			const dailySeriesComponent = component.find(DailySeries);
+			expect(dailySeriesComponent).toHaveLength(1);
+			expect(dailySeriesComponent.props().dailySeries).toEqual(inputDailySeries);
 		});
 	});
 
 	describe('getAllData', () => {
-		it('should call the resetDailySeries props method', () => {
+		it('should call the setState twice enablind/disabling the loading', () => {
+			const spy = spyOn(component, 'setState');
+			component.instance().getAllData();
+			expect(spy).toHaveBeenCalledTimes(2);
+			expect(spy).toHaveBeenNthCalledWith(1, { isLoadingData: true });
+			expect(spy).toHaveBeenNthCalledWith(2, { isLoadingData: false });
+		});
+
+		it('should call the resetDailySeries prop method', () => {
 			component.instance().getAllData();
 			expect(mockResetDailySeries).toHaveBeenCalled();
 		});
 
-		it('should call the fetchDailySeries props method', () => {
+		it('should call the fetchDailySeries with params', () => {
 			component.instance().getAllData();
-			expect(mockFetchDailySeries).toHaveBeenCalled();
+			expect(mockFetchDailySeries).toHaveBeenCalledWith(6, expect.any(Function));
 		});
 	});
 });
