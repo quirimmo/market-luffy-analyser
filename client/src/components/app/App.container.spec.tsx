36bf18d46261d25ed7fbdf1bb943aee6959d0d93
Quirino Lo Russo <quirimmo@gmail.com>
2018-09-18 22:09:16 +0200

    tested app container components

--- client/src/components/app/App.container.spec.tsx
+++ client/src/components/app/App.container.spec.tsx
@@ -3,8 +3,11 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import AppPage from './App.container';
+import * as companiesActions from './../../actions/companies.action';
+import thunk from 'redux-thunk';
 
-const mockStore = configureMockStore();
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 const store = mockStore({});
 let component: any;
 
@@ -20,6 +23,15 @@ describe('App Container Component', () => {
 	it('should define the props', () => {
 		expect(typeof component.props().connectToSocket).toEqual('function');
 		expect(typeof component.props().disconnectFromSocket).toEqual('function');
+		expect(typeof component.props().fetchCompanies).toEqual('function');
+	});
+
+	describe('fetchCompanies', () => {
+		it('should call the fetchCompaniesThunk', () => {
+			const spy = spyOn(companiesActions, 'fetchCompaniesThunk').and.returnValue({ type: 'MOCKED_ACTION' });
+			component.props().fetchCompanies();
+			expect(spy).toHaveBeenCalled();
+		});
 	});
 
 	describe('connectToSocket', () => {
