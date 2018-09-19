29ab55f5822ee362fda1f6a7c25d20ad417d1fea
Quirino Lo Russo <quirimmo@gmail.com>
2018-09-19 23:34:58 +0200

    added unit tests to number formatter

--- client/src/components/shared/NumberFormatter.component.tsx
+++ client/src/components/shared/NumberFormatter.component.tsx
@@ -19,12 +19,12 @@ class NumberFormatter extends React.Component<INumberFormatterProps, any> {
 	public formatValue(): string {
 		let ret: string = '';
 		const stringValue: string = this.props.value.toString();
-		const decimalValues: string = stringValue.split('.')[1];
+		const decimalValues: string = stringValue.split('.')[1] || '00';
 		const integerValues: string = stringValue.split('.')[0];
 		const formattedIntegers: string[] = [];
 		for (let i = integerValues.length - 1, j = 1; i >= 0; i--, j++) {
 			formattedIntegers.unshift(integerValues.charAt(i));
-			if (j % 3 === 0 && j < integerValues.length - 1) {
+			if (j % 3 === 0 && j < integerValues.length) {
 				formattedIntegers.unshift(',');
 			}
 		}
